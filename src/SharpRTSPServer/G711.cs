9a3cf61c833bfb9c39f479504458a1b83a4039fb
Lukas Volf <jimm98y@hotmail.com>
2024-06-15 01:01:59 +0200

    F Added G711

---
+++ src/SharpRTSPServer/G711.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace SharpRTSPServer
+{
+    public class PCMU : ITrack
+    {
+        public int ID { get; set; } = 1;
+        public int SamplingRate { get; } = 8000;
+        public int Channels { get; } = 1;
+
+        public bool IsReady { get { return true; } }
+
+        public int PayloadType => 0;
+
+        public StringBuilder BuildSDP(StringBuilder sdp)
+        {
+            sdp.Append($"m=audio 0 RTP/AVP {PayloadType}\n"); // <---- Payload Type 0 means G711 ULAW
+            sdp.Append($"a=control:trackID={ID}\n");
+            sdp.Append($"a=rtpmap:{PayloadType} PCMU/{SamplingRate}\n");
+            return sdp;
+        }
+    }
+
+    public class PCMA : ITrack
+    {
+        public int ID { get; set; } = 1;
+        public int SamplingRate { get; } = 8000;
+        public int Channels { get; } = 1;
+
+        public bool IsReady { get { return true; } }
+
+        public int PayloadType => 8;
+
+        public StringBuilder BuildSDP(StringBuilder sdp)
+        {
+            sdp.Append($"m=audio 0 RTP/AVP {PayloadType}\n"); // <---- Payload Type 8 means G711 ALAW
+            sdp.Append($"a=control:trackID={ID}\n");
+            sdp.Append($"a=rtpmap:{PayloadType} PCMA/{SamplingRate}\n");
+            return sdp;
+        }
+    }
+}
