23a1872c00bf3529757f9839f124de7eae4c8678
Lukas Volf <jimm98y@hotmail.com>
2024-06-15 21:58:56 +0200

    F Added support for playing H265 in the sample app

--- src/RTSPServerApp/Program.cs
+++ src/RTSPServerApp/Program.cs
@@ -21,7 +21,8 @@ using (var server = new RTSPServer(port, userName, password))
     TrakBox audioTrackBox = null;
     TrakBox videoTrackBox = null;
     AudioSampleEntryBox audioSampleEntry = null;
-    double videoFrameRate = 24;
+    double videoFrameRate = 0;
+    int audioSamplingRate = 0;
 
     // frag_bunny.mp4 audio is not playable in VLC on Windows 11 (works on MacOS)
     using (Stream fs = new BufferedStream(new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read)))
@@ -37,12 +38,39 @@ using (var server = new RTSPServer(port, userName, password))
             {
                 videoTrackId = fmp4.FindVideoTrackID().First();
                 videoFrameRate = fmp4.CalculateFrameRate(videoTrackBox);
+
+                var h264VisualSample = videoTrackBox.GetMdia().GetMinf().GetStbl().GetStsd().Children.FirstOrDefault(x => x.Type == VisualSampleEntryBox.TYPE3 || x.Type == VisualSampleEntryBox.TYPE4) as VisualSampleEntryBox;
+                if (h264VisualSample != null)
+                {
+                    var avcC = (h264VisualSample.Children.First(x => x.Type == AvcConfigurationBox.TYPE) as AvcConfigurationBox).AvcDecoderConfigurationRecord;
+                    server.VideoTrack = new SharpRTSPServer.H264Track(avcC.AvcProfileIndication, 0, avcC.AvcLevelIndication);
+                }
+                else
+                {
+                    var h265VisualSample = videoTrackBox.GetMdia().GetMinf().GetStbl().GetStsd().Children.FirstOrDefault(x => x.Type == VisualSampleEntryBox.TYPE6 || x.Type == VisualSampleEntryBox.TYPE7) as VisualSampleEntryBox;
+                    if(h265VisualSample != null)
+                    {
+                        server.VideoTrack = new SharpRTSPServer.H265Track();
+                    }
+                    else
+                    {
+                        throw new NotSupportedException("No supported video found!");
+                    }
+                }                                
             }
             
             if (audioTrackBox != null)
             {
                 audioTrackId = fmp4.FindAudioTrackID().First();
                 audioSampleEntry = audioTrackBox.GetAudioSampleEntryBox();
+
+                var aac = audioTrackBox.GetMdia().GetMinf().GetStbl().GetStsd().Children.FirstOrDefault(x => x.Type == AudioSampleEntryBox.TYPE3) as AudioSampleEntryBox;
+                if (aac != null)
+                {
+                    var audioConfigDescriptor = audioSampleEntry.GetAudioSpecificConfigDescriptor();
+                    audioSamplingRate = audioConfigDescriptor.GetSamplingFrequency();
+                    server.AudioTrack = new SharpRTSPServer.AACTrack(await audioConfigDescriptor.ToBytes(), audioSamplingRate, audioConfigDescriptor.ChannelConfiguration);
+                }
             }
         }
     }
@@ -56,15 +84,20 @@ using (var server = new RTSPServer(port, userName, password))
     {
         var videoSamplingRate = SharpRTSPServer.H264Track.DEFAULT_CLOCK;
         var videoSampleDuration = videoSamplingRate / videoFrameRate;
-        var videoTrack = parsedMDAT[videoTrackId];
-        var rtspVideoTrack = new SharpRTSPServer.H264Track();
-        server.VideoTrack = rtspVideoTrack;
+        var videoTrack = parsedMDAT[videoTrackId];        
         videoTimer = new Timer(videoSampleDuration * 1000 / videoSamplingRate);
         videoTimer.Elapsed += (s, e) =>
         {
             if (videoIndex == 0)
             {
-                rtspVideoTrack.SetParameterSets(videoTrack[0][0], videoTrack[0][1]);
+                if (server.VideoTrack is SharpRTSPServer.H264Track h264VideoTrack)
+                {
+                    h264VideoTrack.SetParameterSets(videoTrack[0][0], videoTrack[0][1]);
+                }
+                else if (server.VideoTrack is SharpRTSPServer.H265Track h265VideoTrack)
+                {
+                    h265VideoTrack.SetParameterSets(videoTrack[0][0], videoTrack[0][1], videoTrack[0][2]);
+                }
                 videoIndex++;
             }
 
@@ -81,11 +114,6 @@ using (var server = new RTSPServer(port, userName, password))
     {
         var audioSampleDuration = SharpMp4.AACTrack.AAC_SAMPLE_SIZE;
         var audioTrack = parsedMDAT[audioTrackId];
-        var audioConfigDescriptor = audioSampleEntry.GetAudioSpecificConfigDescriptor();
-        var audioSamplingRate = audioConfigDescriptor.GetSamplingFrequency();
-        var rtspAudioTrack = new SharpRTSPServer.AACTrack(audioSamplingRate, audioConfigDescriptor.ChannelConfiguration);
-        rtspAudioTrack.SetConfigDescriptor(await audioConfigDescriptor.ToBytes());
-        server.AudioTrack = rtspAudioTrack;
         audioTimer = new Timer(audioSampleDuration * 1000 / audioSamplingRate);
         audioTimer.Elapsed += (s, e) =>
         {
