eae6e024a1852ae81dbed0f60deb89d34a572511
Lukas Volf <jimm98y@hotmail.com>
2024-06-15 19:21:37 +0200

    R Renaming

--- src/SharpRTSPClient/AACStreamConfigurationDat.cs
+++ src/SharpRTSPClient/AACStreamConfigurationDat.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace SharpRTSPClient
+﻿namespace SharpRTSPClient
 {
     public class AACStreamConfigurationData : IStreamConfigurationData
     {
