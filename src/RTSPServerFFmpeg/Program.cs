23f8805413f1f815a4d3d04a50a6481224c4b632
Lukas Volf <jimm98y@hotmail.com>
2024-08-05 23:26:44 +0200

    F Added ffmpeg process start and SDP parsing from the output

--- src/RTSPServerFFmpeg/Program.cs
+++ src/RTSPServerFFmpeg/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Extensions.Configuration;
 using SharpRTSPServer;
 using System;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
 
 IConfiguration config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
 string hostName = config["HostName"];
@@ -8,30 +11,37 @@ ushort port = ushort.Parse(config["Port"]);
 string userName = config["UserName"];
 string password = config["Password"];
 
-// ffmpeg.exe -re -stream_loop -1 -i C:\Git\SharpMediaCoder\src\SharpMediaPlayer\frag_bunny.mp4 -vcodec copy -an -f rtp rtp://127.0.0.1:11111 -vn -acodec copy -f rtp rtp://127.0.0.1:11113
+string ffmpegPath = config["FFmpegPath"];
+string ffmpegArgs = config["FFmpegArgs"];
+string videoUri = config["VideoUri"];
+string audioUri = config["AudioUri"];
+
+// ffmpeg.exe -re -stream_loop -1 -i frag_bunny.mp4 -vcodec copy -an -f rtp rtp://127.0.0.1:11111 -vn -acodec copy -f rtp rtp://127.0.0.1:11113
+ProcessStartInfo info = new ProcessStartInfo();
+info.FileName = ffmpegPath;
+info.Arguments = ffmpegArgs;
+info.RedirectStandardOutput = true;
+info.UseShellExecute = false;
+
+string lastLine = null;
+SemaphoreSlim semaphore = new SemaphoreSlim(0);
+StringBuilder sdp = new StringBuilder();
+var process = Process.Start(info);
+process.OutputDataReceived += Process_OutputDataReceived;
+process.BeginOutputReadLine();
+
+// wait until the SDP is read
+semaphore.Wait();
+
 using (var server = new RTSPServer(port, userName, password))
 {
-    server.AddVideoTrack(new ProxyTrack(0, "rtp://127.0.0.1:11111"));
-    server.AddAudioTrack(new ProxyTrack(1, "rtp://127.0.0.1:11113"));
-    // original
-    string sdp =
-        "v=0\r\n" +
-        "o=- 0 0 IN IP4 127.0.0.1\r\n" +
-        "s=No Name\r\n" +
-        "t=0 0\r\n" +
-        "a=tool:libavformat 60.3.100\r\n" +
-        "m=video 11111 RTP/AVP 96\r\n" +
-        "c=IN IP4 127.0.0.1\r\n" +
-        "b=AS:587\r\n" +
-        "a=rtpmap:96 H264/90000\r\n" +
-        "a=fmtp:96 packetization-mode=1; sprop-parameter-sets=Z2QAHqzZQKAv+WagwCDW4AAAAwAgAAAGAeLFssA=,aOvjyyLA; profile-level-id=64001E\r\n" +
-        "m=audio 11113 RTP/AVP 97\r\n" +
-        "c=IN IP4 127.0.0.1\r\n" +
-        "b=AS:69\r\n" +
-        "a=rtpmap:97 MPEG4-GENERIC/44100/1\r\n" +
-        "a=fmtp:97 profile-level-id=1;mode=AAC-hbr;sizelength=13;indexlength=3;indexdeltalength=3; config=120856E500\r\n\r\n";    
+    if (!string.IsNullOrEmpty(videoUri))
+        server.AddVideoTrack(new ProxyTrack(0, videoUri));
 
-    server.OverrideSDP(sdp, true);
+    if (!string.IsNullOrEmpty(audioUri))
+        server.AddAudioTrack(new ProxyTrack(1, audioUri));
+
+    server.OverrideSDP(sdp.ToString(), true);
 
     try
     {
@@ -48,5 +58,23 @@ using (var server = new RTSPServer(port, userName, password))
     while (!Console.KeyAvailable)
     {
         System.Threading.Thread.Sleep(250); 
+    }
+
+    process.Kill();
+}
+
+void Process_OutputDataReceived(object sender, DataReceivedEventArgs e)
+{
+    if(lastLine == "" && e.Data == "")
+    {
+        semaphore.Release();
+        return;
+    }
+
+    lastLine = e.Data;
+
+    if (!string.IsNullOrEmpty(e.Data) && !e.Data.StartsWith("SDP:"))
+    {
+        sdp.AppendLine(e.Data);
     }
 }
