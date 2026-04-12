ba3bb9cb8b8f1212f1d12f17809d6a92490eff41
Lukas Volf <jimm98y@hotmail.com>
2026-04-12 16:22:17 +0200

    F Updated SharpMP4, fixed breaking changes

--- src/RTSPServerApp/Program.cs
+++ src/RTSPServerApp/Program.cs
@@ -2,15 +2,6 @@
 using Microsoft.Extensions.Hosting;
 using RTSPServerApp;
 
-SharpH26X.Log.SinkDebug = (o, e) => { };
-SharpH26X.Log.SinkInfo = (o, e) => { };
-SharpAV1.Log.SinkInfo = (o, e) => { };
-SharpAV1.Log.SinkDebug = (o, e) => { };
-SharpISOBMFF.Log.SinkInfo = (o, e) => { };
-SharpISOBMFF.Log.SinkDebug = (o, e) => { };
-SharpMP4.Log.SinkInfo = (o, e) => { };
-SharpMP4.Log.SinkDebug = (o, e) => { };
-
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<RTSPServerWorker>();
 
