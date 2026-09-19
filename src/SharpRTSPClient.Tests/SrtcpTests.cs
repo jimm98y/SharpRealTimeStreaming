21f606af66a7b8ea87e779fecab5ec52c6357c41
Lukas Volf <jimm98y@hotmail.com>
2026-09-19 16:53:54 +0200

    F Added headers

--- src/SharpRTSPClient.Tests/SrtcpTests.cs
+++ src/SharpRTSPClient.Tests/SrtcpTests.cs
@@ -1,7 +1,26 @@
-using System;
+// SharpRTSPClient
+// Copyright (C) 2026 Lukas Volf
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 using System.Linq;
 using SharpSRTP.SRTP;
-using SharpRTSPClient;
 
 namespace SharpRTSPClient.Tests
 {
@@ -82,7 +101,7 @@ namespace SharpRTSPClient.Tests
             byte[] first = RTSPClient.ProtectRtcp(context, ReceiverReport(0x11111111));
             byte[] second = RTSPClient.ProtectRtcp(context, ReceiverReport(0x22222222));
 
-            Assert.AreNotEqual(first, second);
+            Assert.AreNotSequenceEqual(first, second);
         }
     }
 }
